a141f3714505b572075792f9f39a31cce8cc94a8
Greger Stolt Nilsen <greger@fusetools.com>
2021-10-07 21:27:10 +0200

    More build stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# This Happened
+
+A tool made for creating handouts for playing table top RPGs.
+
+Reads files containing markdown, and renders to an image.
+
+## TODO
+
+- Write some documentation.
--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="thishappened",
     version='0.1',
     author="Greger Stolt Nilsen",
     author_email="gregersn@gmail.com",
     description="A utility to convert markdown to images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
